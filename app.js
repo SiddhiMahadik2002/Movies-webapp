168d042b380b8b065458fb3d60be73e1d3ee0f6b
Siddhi <siddhimahadik257@gmail.com>
2022-09-10 15:52:20 +0530

    final version released 1.0.0

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 // https://ott-details.p.rapidapi.com/advancedsearch
 /*'X-RapidAPI-Key': '4461487b5emsh9ccbc9c32e91a0ep1a89f4jsnb17e07e0fb9a',
         'X-RapidAPI-Host': 'ott-details.p.rapidapi.com'*/
+        let moviesSec=document.querySelector(".moviessec")
+        let searchBox=document.querySelector(".input")
+        let button=document.querySelector(".btn")
 const options = {
     method: 'GET',
     headers: {
@@ -9,18 +12,54 @@ const options = {
     }
 };
 
+
 function getMovies() {
-    fetch("https://ott-details.p.rapidapi.com/advancedsearch", options).then(response => response.json()).then(response => {
+    fetch("https://ott-details.p.rapidapi.com/advancedsearch", options).then(res => res.json()).then(data => {
         let htmlstring=""
-        response.results.map((item,index)=>{
-        //     console.log(item.title)
+        console.log(data)
+        data.results.map((item,index)=>{
+            htmlstring+=`<div class="card">
+            <img src="${item.imageurl[0]}"
+                alt="">
+            <h3 class="movietitle">${item.title}</h3>
+            <p>${item.released + " - "+item.type}</p>
+            <p>${item.synopsis}</p>
+        </div>
+        `
         })
-        // console.log(response)
+        console.log(data)
+        moviesSec.innerHTML=htmlstring
     })
 
 }
+
 getMovies()
-name1=""
-name2="siddhi"
-name3="jfkvn"
-console.log(name1+name2+name3)
+function searchMovies(movie){
+    console.log(movie)
+fetch(`https://ott-details.p.rapidapi.com/search?title=${movie}&page=1`,options).then(response=>response.json()).then(data=>{
+    let htmlstring=""
+    console.log(data)
+    data.results.map((item,index)=>{
+        htmlstring+=`<div class="card">
+        <img src="${item.imageurl[0]}"
+            alt="">
+        <h3 class="movietitle">${item.title}</h3>
+        <p>${item.released + " - "+item.type}</p>
+        <p>${item.synopsis}</p>
+    </div>
+    `
+    })
+    console.log(data)
+    moviesSec.innerHTML=htmlstring
+    })
+}
+button.addEventListener("click",()=>{
+    if(searchBox.value.length==0){
+        getMovies()
+    }
+    else{
+        
+        searchMovies(searchBox.value)
+    }
+})
+
